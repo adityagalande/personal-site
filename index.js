2eab77131497890efadf7747b01875c389125a63
Aditya Galande <adityagalande14@gmail.com>
2021-06-17 16:16:47 +0530

    navBar back & transition dynamiclly added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// alert("Fresh");
+const nav = document.querySelector('#navigationBar');
+const guggu = document.querySelector('.innerTitle');
+const topNav = guggu.offsetTop;
+
+function animNav() {
+  console.log(window.scrollY + nav.offsetHeight);
+  if (window.scrollY + nav.offsetHeight >= topNav) {
+    document.getElementById('navigationBar').style.backgroundColor = '#28282B';
+    document.getElementById("navigationBar").style.transition = "all 0.7s";
+  } else {
+    document.getElementById('navigationBar').style.backgroundColor = 'transparent';
+    document.getElementById("navigationBar").style.transition = "all 0.7s";
+  }
+}
+
+window.addEventListener('scroll', animNav);
